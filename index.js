c5eeb07dc5c329ca2698f39c8879090747f635b2
Hassan222-pixel <hassanawadk123@gmail.com>
2024-11-15 09:04:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const nav = document.getElementById("navbar");
+
+
+if(bar){
+    bar.addEventListener("click", function(){
+        nav.classList.add("hamburger");
+    })
+}
+
+ if(close){
+    close.addEventListener("click", function(){
+        nav.classList.remove("hamburger");
+    })
+}
+
